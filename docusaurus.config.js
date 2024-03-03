8975c89409f48f7952797883bb2cdc8e41e1ddf1
Diego-Palomino-Jjjeeee <palominodiego210@gmail.com>
2024-03-03 15:42:41 -0500

    Update baseurl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://Diego-Palomino-Jjjeeee.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/buho/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   organizationName: 'Diego-Palomino-Jjjeeee',
